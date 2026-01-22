727716151779f0523ccba3622eedc84b44912560
Jatin Sharma <jatina9410@gmail.com>
2026-01-23 01:25:36 +0530

    feat : initilized server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-console.log("hello ");
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express();
+app.use(cors()) ;
+app.use(express.json()) ;
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/',(req,res)=>{
+    res.json({
+        msg:"hello from ther server"
+    });
+    
+})
+
+app.listen(PORT,()=>{
+console.log(`Server is running on http://localhost:${PORT}` );
+
+})
