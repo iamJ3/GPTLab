1b6ee8fd0d98510387cc4844d5e4ee9ab32b6f80
Jatin Sharma <jatina9410@gmail.com>
2026-01-24 01:52:10 +0530

    feat : intilized clerk in backend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 app.use(cors()) ;
 app.use(express.json()) ;
+app.use(clerkMiddleware()) ;
 
 const PORT = process.env.PORT || 8000;
 
@@ -17,5 +19,5 @@ app.get('/',(req,res)=>{
 
 app.listen(PORT,()=>{
 console.log(`Server is running on http://localhost:${PORT}` );
-
+//ai saaas pern app greatstack
 })
