5b7e6980053db474f6515bd84c0706dc147f88c2
Jatin Sharma <jatina9410@gmail.com>
2026-01-27 02:50:45 +0530

    feat : added axios for gemini calls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import AppRoutes from './routes/AppRoutes.jsx'
+import { useAuth } from '@clerk/clerk-react'
+import { useEffect } from 'react';
 
 const App = () => {
+  const {getToken} = useAuth();
+ useEffect(() => {
+  getToken().then((token)=>console.log(token));
+ 
+ }, []);
   return (
     <div>
       <AppRoutes/>
